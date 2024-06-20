38d355f0b571e44395fa84c417b846ccb87a187b
Artur <arturrodriguesmota@hotmail.com>
2024-06-20 14:39:55 -0300

    INITIAL: Basic template with react js and scss

---
+++ reactjs-scss-template/src/App.jsx
@@ -0,0 +1,12 @@
+import s from './app.module.scss'
+
+function App() {
+
+  return (
+    <main className={s.text}>
+      <h1>Texto Para Teste - Design System</h1>
+    </main>
+  )
+}
+
+export default App
